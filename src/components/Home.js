f2e114e06f21639992d7b1d1824aba95c45a068e
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-01-15 15:30:33 +0300

    second update

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,12 @@ import { useContext } from "react";
 import { DataContext } from "../context/DataContext";
 
 const Home = () => {
-    const {posts, fetchError, isLoading} = useContext(DataContext);
+    const {searchResults, fetchError, isLoading} = useContext(DataContext);
     return (
         <main className="Home">
             {isLoading && <p className="statusMsg">Loading posts...</p>}
             {!isLoading && fetchError && <p className="statusMsg" style={{color: "red"}}>{fetchError}</p>}
-            {!isLoading && !fetchError && (posts.length ? <Feed posts={posts}/> : <p className="statusMsg"> No posts to display</p>) }
+            {!isLoading && !fetchError && (searchResults.length ? <Feed posts={searchResults}/> : <p className="statusMsg"> No posts to display</p>) }
         </main>
     )
 }
